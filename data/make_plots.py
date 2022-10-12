fd1d6eda062f7268cd373d4d1ed385c3a81e38e6
Simeon Bird <sbird@ucr.edu>
2022-10-11 20:18:47 -0700

    Plot showing box size convergence

--- data/make_plots.py
+++ data/make_plots.py
@@ -7,6 +7,53 @@ import matplotlib.pyplot as plt
 from lyaemu.likelihood import LikelihoodClass
 import lyaemu.distinct_colours_py3 as dc
 
+def make_box_convergence(convfile):
+    """Make a plot showing the convergence of the flux power spectrum with box size."""
+    hh = h5py.File(convfile)
+    #Low-res is current.
+    flux_powers_lr = hh["flux_powers"]["L120n1024"][:]
+    flux_powers_hr = hh["flux_powers"]["L60n512"][:]
+    kfkms_vhr = hh["kfkms"][:]
+    redshifts = hh["zout"][:]
+    nk = np.shape(kfkms_vhr)[0]
+    flux_powers_lr = flux_powers_lr.reshape((nk,-1))
+    flux_powers_hr = flux_powers_hr.reshape((nk,-1))
+    fig = plt.figure()
+    axes = []
+    index = 1
+    for ii, zz in enumerate(redshifts):
+        if zz > 4.4 or zz < 2.2:
+            continue
+        sharex=None
+        sharey=None
+        if index > 3:
+            sharex = axes[(index-1) % 3]
+        #if (index-1) % 3 > 0:
+            #sharey = axes[index -1 - ((index-1) % 3)]
+        ax = fig.add_subplot(4,3, index, sharex=sharex, sharey=sharey)
+        if sharex is None:
+             ax.set_xlim(1.5e-3, 2e-2)
+        ax.semilogx(kfkms_vhr[ii], flux_powers_lr[ii]/flux_powers_hr[ii], color="blue", ls="-")
+        ax.text(2.5e-3, 0.97, "z=%.2g" % zz)
+        ax.set_ylim(0.95, 1.05)
+        ax.grid(visible=True, axis='y')
+        if (index-1) % 3 > 0:
+            ax.set_yticks([0.98,1.0, 1.02]) #, [str(0.97), str(1.0), str(1.03)])
+            ax.set_yticklabels([])
+        else:
+            ax.set_yticks([0.98,1.0, 1.02]) #, [str(0.97), str(1.0), str(1.03)])
+            plt.ylabel(r"$P_F / P_F^{ref}$")
+#         if index > 8:
+#             ax.set_xlim(2e-3, 2e-2)
+#         if index > 8:
+#             ax.set_xticklabels([])
+#             ax.set_xticks([2e-3,5e-3,0.01,2e-2])
+        plt.xlabel("k (s/km)")
+        axes.append(ax)
+        index += 1
+    plt.subplots_adjust(wspace=0, hspace=0)
+    plt.savefig("../figures/box-convergence.pdf")
+
 def make_res_convergence(convfile):
     """Make a plot showing the convergence of the flux power spectrum with resolution."""
     hh = h5py.File(convfile)
@@ -47,46 +94,6 @@ def make_res_convergence(convfile):
     plt.subplots_adjust(wspace=0, hspace=0)
     plt.savefig("../figures/resolution-convergence.pdf")
 
-
-def make_box_convergence(convfile):
-    """Make a plot showing the convergence of the flux power spectrum with box size."""
-    hh = h5py.File(convfile)
-    #Low-res is current.
-    flux_powers_lr = hh["flux_vectors"]["L15n192"][:]
-    kfkms_lr = hh["kfkms"]["L15n192"][:]
-    flux_powers_hr = hh["flux_vectors"]["L15n384"][:]
-    kfkms_hr = hh["kfkms"]["L15n384"][:]
-    flux_powers_vhr = hh["flux_vectors"]["L15n512"][:]
-    kfkms_vhr = hh["kfkms"]["L15n512"][:]
-    redshifts = hh["zout"][:]
-    fig = plt.figure()
-    axes = []
-    index = 1
-    for ii, zz in enumerate(redshifts):
-        if zz > 4.4 or zz < 2.2:
-            continue
-        sharex=None
-        sharey=None
-        if index > 3:
-            sharex = axes[(index-1) % 3]
-        #if (index-1) % 3 > 0:
-            #sharey = axes[index -1 - ((index-1) % 3)]
-        ax = fig.add_subplot(4,3, index, sharex=sharex, sharey=sharey)
-        ax.semilogx(kfkms_vhr[ii], flux_powers_lr[ii]/flux_powers_vhr[ii], label="%.2g kpc/h" % (15000./192.), color="blue", ls="-")
-        ax.semilogx(kfkms_vhr[ii], flux_powers_hr[ii]/flux_powers_vhr[ii], label="%.2g kpc/h" % (15000./384.), color="grey", ls="--")
-        ax.text(kfkms_vhr[ii][0]*1.3, 1.02, "z="+str(zz))
-        ax.set_ylim(1.0, 1.05)
-        if (index-1) % 3 > 0:
-            ax.set_yticklabels([])
-        else:
-            ax.set_yticks([1.0, 1.02, 1.04], [str(1.0), str(1.02), str(1.04)])
-        if index == 1:
-            ax.legend()
-        axes.append(ax)
-        index += 1
-    plt.subplots_adjust(wspace=0, hspace=0)
-    plt.savefig("../figures/resolution-convergence.pdf")
-
 def make_temperature_variation(tempfile, ex=5, gkfile="Gaikwad_2020b_T0_Evolution_All_Statistics.txt"):
     """Make a plot of the possible temperature variations over time."""
     obs = np.loadtxt(gkfile)
@@ -110,7 +117,7 @@ def save_fig(name, plotdir):
     #plt.ylim(bottom=0.9, top=1.1)
     plt.xlabel(r"$k_F$")
     plt.ylabel(r"$\Delta P_F(k)$ ($%s$)" % name[1])
-    plt.legend(loc="lower left", ncol=3,fontsize=10)
+    plt.legend(loc="lower left", ncol=1,fontsize=10)
     plt.tight_layout()
     plt.savefig(os.path.join(plotdir,"single_param_%s.pdf" % name[0]))
     plt.clf()
@@ -124,7 +131,7 @@ def single_parameter_plot(zzs=None, plotdir='../figures'):
     okf, defaultfv, _ = like.get_predicted(means)
     pnames = like.get_pnames()
     if zzs is None:
-        zzs = np.array([2.2, 3.0, 4.4])
+        zzs = np.array([2.2, 3.2, 4.4])
     assert len(pnames) == np.size(means)
     dist_col = dc.get_distinct(12)
     for (i, name) in enumerate(pnames):
@@ -141,8 +148,10 @@ def single_parameter_plot(zzs=None, plotdir='../figures'):
             plt.semilogx(okf[zind], upperfv[zind]/defaultfv[zind], label=r"$%s=%.2g$, $z=%.2g$" % (name[1], upper[i], zz), color=dist_col[2*j % 12])
             plt.semilogx(okf[zind], lowerfv[zind]/defaultfv[zind], label=r"$%s=%.2g$, $z=%.2g$" % (name[1], lower[i], zz), ls="--", color=dist_col[(2*j+1) %12])
         save_fig(name, plotdir)
+    return like
 
 if __name__ == "__main__":
     make_temperature_variation("emulator_meanT.hdf5-40")
     make_res_convergence("fluxpower_converge.hdf5")
+    make_box_convergence("box_converge.hdf5")
     single_parameter_plot()
